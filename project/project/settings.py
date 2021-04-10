9905099ea49de7432d59aee796791c7d2d48dd8c
João Paulo Carvalho <joaopaulo.carvalho@br.experian.com>
2021-04-10 17:24:53 -0300

    cadastrando modelo de cliente e registrando no Admin

--- project/project/settings.py
+++ project/project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gestor',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'gestor.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'gestor.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
